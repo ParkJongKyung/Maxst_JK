ffdb33a7d4979d81d6c48b819f8060a955d46ee2
DESKTOP-7PID0GH\Park Jong Kyung <creatingpjk@gmail.com>
2018-08-29 14:51:43 +0900

    Test Commit

---
+++ Maxst/Assets/01. Scripts/AbstractBackScene.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class Back : MonoBehaviour {
+    protected GameObject ObjPopup;
+
+    public abstract string PopSceneName { get; }
+    public abstract void PushSceneName(string SceneName);
+    public abstract void EscapeScene();
+}
+
+public class AbstractBackScene : Back
+{
+    public static Stack<string> SceneStack = new Stack<string>();
+
+    public override string PopSceneName
+    {
+        get { return SceneStack.Pop(); }
+    }
+
+    public override void PushSceneName(string SceneName)
+    {
+        SceneStack.Push(SceneName);
+    }
+
+    public override void EscapeScene()
+    {
+        string EscapeSceneName = SceneStack.Pop();
+        if (EscapeSceneName != "Popup")
+        {
+            SceneManager.LoadScene(EscapeSceneName);
+        }
+        else
+        {
+            DestroyImmediate(ObjPopup);
+        }        
+    }
+
+    public void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneStack.Count != 0)
+            {
+                EscapeScene();
+            }
+        }    
+    }
+}
---
+++ Maxst/Assets/01. Scripts/SceneChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChange : AbstractBackScene
+{   
+    public void NextScene(string SceneName) {
+        PushSceneName(Application.loadedLevelName);
+        if (SceneName != "Popup")
+        {            
+            SceneManager.LoadScene(SceneName);
+        }
+        else
+        {            
+            ObjPopup = Instantiate(Resources.Load<GameObject>("Popup"), GameObject.Find("Canvas").transform);            
+        }        
+    }
+}
